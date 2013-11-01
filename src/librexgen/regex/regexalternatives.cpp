df85d0ba616e97eaf6984d5f037d2e659e2d7cab
jan.starke@outofbed.org <jan.starke@outofbed.org@c03ab62d-064e-604f-90e8-bc6f304d3419>
2013-11-01 21:08:39 +0000

    manpage for rexgen, license change to GPLv2
    git-svn-id: http://rexgen.googlecode.com/svn/trunk@73 c03ab62d-064e-604f-90e8-bc6f304d3419

--- src/librexgen/regex/regexalternatives.cpp
+++ src/librexgen/regex/regexalternatives.cpp
@@ -1,18 +1,20 @@
 /*
-    Copyright (C) 2012-2013  Jan Starke <rexgen@outofbed.org>
+    rexgen - a tool to create words based on regular expressions    
+    Copyright (C) 2012-2013  Jan Starke <jan.starke@outofbed.org>
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+    This program is free software; you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the Free
+    Software Foundation; either version 2 of the License, or (at your option)
+    any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+    This program is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+    more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin St, Fifth Floor, Boston, MA 02110, USA
 */
 
 #include <librexgen/iterator/iteratorstate.h>
--- src/librexgen/version.h
+++ src/librexgen/version.h
@@ -1,32 +1,34 @@
 /*
-Copyright (C) 2012-2013  Jan Starke <rexgen@outofbed.org>
+    rexgen - a tool to create words based on regular expressions    
+    Copyright (C) 2012-2013  Jan Starke <jan.starke@outofbed.org>
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+    This program is free software; you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the Free
+    Software Foundation; either version 2 of the License, or (at your option)
+    any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+    This program is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+    more details.
 
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin St, Fifth Floor, Boston, MA 02110, USA
 */
 
 #ifndef __VERSION_H__
 #define __VERSION_H__
-#include <librexgen\parser\osdepend.h>
+#include <librexgen/parser/osdepend.h>
 #include <cstdio>
 #include <cstdlib>
 
 using namespace std;
 
 namespace rexgen {
-	static const int VERSION_MAJOR = 1;
-	static const int VERSION_MINOR = 0;
-	static const char* VERSION_REVISION = "$Rev$";
+	const int VERSION_MAJOR = 1;
+	const int VERSION_MINOR = 0;
+	const char* VERSION_REVISION = "$Rev$";
 
 	EXPORT
 	const char* getVersion();
