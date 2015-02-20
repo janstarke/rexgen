64ec7e808f15c12a8fa19c56305c339e88c9de8c
Jan Starke <jasa@w4deumsy9000332.local>
2015-02-20 10:30:38 +0100

    python module for rexgen

--- src/librexgen-python/sample.py
+++ src/librexgen-python/sample.py
@@ -1,51 +1,5 @@
 from ctypes import *
-rexgen = cdll.LoadLibrary("librexgen-c.dylib")
-
-class SimpleString:
-	def __init__(self):
-		self.c_simplestring_new = rexgen.c_simplestring_new
-		self.c_simplestring_new.restype = c_void_p
-
-		self.c_simplestring_to_binary_string = rexgen.c_simplestring_to_binary_string
-		self.c_simplestring_to_binary_string.argtypes = [c_void_p, c_char_p, c_uint]
-
-		self.c_simplestring_clear = rexgen.c_simplestring_clear
-		self.c_simplestring_clear.argtypes = [c_void_p]
-		self.c_iterator_value = rexgen.c_iterator_value
-		self.c_iterator_value.argtypes = [c_void_p, c_void_p]
-
-		self.__len = 64
-		self.__buffer = create_string_buffer(self.__len);
-		self.__str = self.c_simplestring_new()
-
-	def next(self, raw_iterator):
-		self.c_simplestring_clear(self.__str)
-		self.c_iterator_value(raw_iterator, self.__str)
-		self.c_simplestring_to_binary_string(self.__str, self.__buffer, self.__len)
-		return self.__buffer.value
-
-class Iterator:
-	def __init__(self, regex):
-		self.c_regex_iterator = rexgen.c_regex_iterator
-		self.c_regex_iterator.restype = c_void_p
-
-		self.c_iterator_next = rexgen.c_iterator_next
-		self.c_iterator_next.argtypes = [c_void_p]
-
-		self.c_iterator_delete = rexgen.c_iterator_delete
-		self.c_iterator_delete.argtypes=[c_void_p]
-
-		self.__iterator = self.c_regex_iterator(regex, 0, 2, 0)
-		self.__str = SimpleString()
-
-	def __iter__(self):
-		return self
-
-	def next(self):
-		if self.c_iterator_next(self.__iterator):
-			return self.__str.next(self.__iterator)
-		else:
-			raise StopIteration()
+from rexgen import *
 
 if __name__ == '__main__':
 	for word in Iterator("abc[1-3]"):
