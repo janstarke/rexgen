901b49188923f0a8c09248a5c4131fe2e25d60e7
Jan Starke <jan.starke@outofbed.org>
2016-09-26 15:11:56 +0200

    added missing bool.h

---
+++ src/librexgen/common/bool.h
@@ -0,0 +1,29 @@
+/*
+    rexgen - a tool to create words based on regular expressions
+    Copyright (C) 2012-2015 Jan Starke <jan.starke@outofbed.org>
+
+    This program is free software; you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the Free
+    Software Foundation; either version 2 of the License, or (at your option)
+    any later version.
+
+    This program is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+    more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin St, Fifth Floor, Boston, MA 02110, USA
+*/
+
+#ifndef _SRC_LIBREXGEN_COMMON_BOOL_H
+#define _SRC_LIBREXGEN_COMMON_BOOL_H
+
+#ifndef __cplusplus
+typedef int bool;
+#define false 0
+#define true 1
+#endif
+
+#endif
