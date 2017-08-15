6bcd281bb366e3b2c438435892c99feb0e6224ae
Jan Starke <jan.starke@t-systems.com>
2017-08-15 11:37:42 +0200

    fixes #51: check for existing Doxyfile.in

--- src/cmake/doxygen.cmake
+++ src/cmake/doxygen.cmake
@@ -1,10 +1,12 @@
 # add a target to generate API documentation with Doxygen
 find_package(Doxygen)
 if(DOXYGEN_FOUND)
-  configure_file(${CMAKE_CURRENT_SOURCE_DIR}/../docs/Doxyfile.in ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile @ONLY)
-  add_custom_target(doc
-    ${DOXYGEN_EXECUTABLE} ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile
-    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
-    COMMENT "Generating API documentation with Doxygen" VERBATIM
-  )
-  endif(DOXYGEN_FOUND)
+  if(EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/../docs/Doxyfile.in")
+    configure_file(${CMAKE_CURRENT_SOURCE_DIR}/../docs/Doxyfile.in ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile @ONLY)
+    add_custom_target(doc
+      ${DOXYGEN_EXECUTABLE} ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile
+      WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+      COMMENT "Generating API documentation with Doxygen" VERBATIM
+    )
+  endif(EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/../docs/Doxyfile.in")
+endif(DOXYGEN_FOUND)
