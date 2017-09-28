30415a8ca6f0ac2ab039be8bfc50efbb93787458
Jan Starke <jan.starke@t-systems.com>
2017-09-28 13:24:31 +0200

    code coverage report is generated automatically

--- build.sh
+++ build.sh
@@ -13,4 +13,16 @@ cd ${BUILDDIR}
 echo "running >>> cmake ${CMAKE_OPTIONS} ${BASEDIR}/src <<<"
 cmake ${CMAKE_OPTIONS} ${BASEDIR}/src && make
 
+if test "${TEST_ENABLED}" -eq "1" -a $? -eq "0"; then
+  lcov --gcov-tool /usr/local/bin/gcov-7 -d . -b . -z
+  make test
+  if test "${COVERAGE_ENABLED}" -eq "1" -a $? -eq "0"; then
+    lcov --gcov-tool /usr/local/bin/gcov-7 -d . -b . -c -o librexgen.info
+    lcov --gcov-tool /usr/local/bin/gcov-7 -d . -b . -r librexgen.info '*.l' -o librexgen.info
+    lcov --gcov-tool /usr/local/bin/gcov-7 -d . -b . -r librexgen.info '*.y' -o librexgen.info
+    genhtml -o test_coverage librexgen.info
+  fi
+fi
+
+
 cd ${OLD_CWD}
