f21e8bc592bc61ce277d8843049b3559cfb0cf02
Jan Starke <jan.starke@t-systems.com>
2019-03-27 00:28:12 +0100

    fixed invalid library path in lua sample

--- src/librexgen/lua/rexgen.lua
+++ src/librexgen/lua/rexgen.lua
@@ -1,5 +1,5 @@
 #!${LUA_BINARY}
-package.cpath = '${CMAKE_INSTALL_PREFIX}/lib/${librexgen-lua}${CMAKE_SHARED_LIBRARY_SUFFIX}'
+package.cpath = '${CMAKE_INSTALL_PREFIX}/lib/${librexgen_lua}${CMAKE_SHARED_LIBRARY_SUFFIX}'
 local rexgen = require '${prj}-lua'
 
 if (#arg ~= 1) then
